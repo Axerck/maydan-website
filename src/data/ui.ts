86cb63d68306b13bc6611be112568f89afe7f2f3
Axerck <luigi.sausa@gmail.com>
2025-12-29 14:32:48 +0100

    feat: implement menu localization with dynamic translations

---
+++ src/data/ui.ts
@@ -0,0 +1,24 @@
+export const ui = {
+    it: {
+        nav: {
+            about: "CHI SIAMO",
+            calls: "CALL E LINEE GUIDA",
+            issues: "NUMERI",
+            editorial_board: "COMITATO EDITORIALE",
+            scientific_board: "COMITATO SCIENTIFICO",
+            activities: "ATTIVITA’",
+            contacts: "CONTATTI"
+        }
+    },
+    en: {
+        nav: {
+            about: "ABOUT US",
+            calls: "CALLS & GUIDELINES",
+            issues: "ISSUES",
+            editorial_board: "EDITORIAL BOARD",
+            scientific_board: "SCIENTIFIC BOARD",
+            activities: "ACTIVITIES",
+            contacts: "CONTACTS"
+        }
+    },
+} as const;
