c36ebfec6ea737d7f009ca2e1b44de1c34835df3
Axerck <luigi.sausa@gmail.com>
2026-03-04 21:12:11 +0100

    chore: add SEO meta tags, sitemap, and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://www.maydan.it',
+  integrations: [react(), sitemap()],
   i18n: {
     defaultLocale: "it",
     locales: ["it", "en"],
     routing: {
-        prefixDefaultLocale: true
+      prefixDefaultLocale: true
     }
   }
 });
